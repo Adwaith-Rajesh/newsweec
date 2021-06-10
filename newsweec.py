4163d7ddaca69104c0fc434bbb36a8ac3898e757
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-06-10 14:52:53 +0530

    Added --init argument

--- newsweec.py
+++ newsweec.py
@@ -1,13 +1,30 @@
 import argparse
+import os
+from contextlib import suppress
+from pathlib import Path
 
 from newsweec.bot.bot import start_bot
 
 
+def make_folder(folder_name: str) -> None:
+    with suppress(FileExistsError):
+        os.mkdir(folder_name)
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--init", help="Creates all the folders and files necessary for the bot.", action="store_true")
     parser.add_argument("--start", help="start the bot", action="store_true")
 
     args = parser.parse_args()
+
+    if args.init:
+        folders = ["db_store", "logs"]
+        for f in folders:
+            make_folder(f)
+        quit()
+
     if args.start:
         start_bot()
 
