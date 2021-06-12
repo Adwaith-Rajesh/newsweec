770daf49cbd9ce4796ef66626215d4087c522ac4
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-06-12 19:51:49 +0530

    Added log statements

--- newsweec/database/users_db.py
+++ newsweec/database/users_db.py
@@ -6,7 +6,17 @@ from typing import Union
 
 from pysondb import db
 
+from newsweec.meta.logger import logging  # noreorder
+from newsweec.meta.logger import Logger  # noreorder
+
 from newsweec.utils._dataclasses import UserDBInfo
+
+
+DEBUG = logging.DEBUG
+
+d_l = logging.getLogger("db_logger")
+db_logger = Logger(d_l, base_level=DEBUG, filename="")
+
 
 DBType = List[Dict[str, Union[int, bool, List[str]]]]
 
@@ -27,6 +37,7 @@ class UsersDB:
         self.db = db.getDb(get_db_path())
 
     def get_all_data(self, ) -> DBType:
+        db_logger.log(DEBUG, message="Call for all data")
         db_data = self.db.getAll()
         return db_data
 
@@ -35,6 +46,9 @@ class UsersDB:
         if self.check_user_exists(user_id):
             user_data = self.db.getBy({"user_id": user_id})[0]
             user_data["db_id"] = user_data.pop("id")
+
+            db_logger.log(DEBUG, message=f"get_user_info({user_id})")
+
             return UserDBInfo(**user_data)
 
         return None
@@ -46,11 +60,13 @@ class UsersDB:
         return: int: the db_id
         """
         if not self.check_user_exists(user_id):
-            db_id = self.db.add({**{
+            data = {**{
                 "user_id": user_id,
                 "feed": True,
                 "topics": ["business", "entertainment", "general", "health", "science", "sports", "technology"]
-            }, **kwargs})
+            }, **kwargs}
+            db_id = self.db.add(data)
+            db_logger.log(DEBUG, message=f"Added user with {user_id=} {data=}")
 
             return db_id
 
@@ -73,6 +89,8 @@ class UsersDB:
 
             new_data = {**default_data_schema, **kwargs}
             self.db.update(default_data_schema, new_data)
+            db_logger.log(
+                DEBUG, message=f"Updated user with {user_id=} {new_data=}")
 
         else:
             rv = self.add_user(user_id, **kwargs)
@@ -82,6 +100,7 @@ class UsersDB:
         """set feed to false"""
         if self.check_user_exists(user_id):
             self.update_user(user_id, feed=False)
+            db_logger.log(DEBUG, message=f"Delete user with {user_id=}")
 
     def check_user_exists(self, user_id: int) -> bool:
         return True if self.db.getBy({"user_id": user_id}) else False
