bd88ffa3a5c9fe0743cb441ebeaa4ab6f4da9dea
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-06-13 17:32:10 +0530

    Redesigned the keyboard, made it easier to make new keyboards

--- newsweec/bot/keyboards.py
+++ newsweec/bot/keyboards.py
@@ -6,20 +6,20 @@ from telebot.types import ReplyKeyboardMarkup
 from newsweec.utils.decorators import get_keyboard_buttons
 
 
-@get_keyboard_buttons("basic")
-def basic_start_keyboard(buttons: List[str] = []) -> ReplyKeyboardMarkup:
+def get_keyboard_markup(name: str):
 
-    markup = ReplyKeyboardMarkup(row_width=2)
-    markup.add(*[KeyboardButton(i) for i in buttons])
-    return markup
+    @get_keyboard_buttons(name)
+    def gen_markup(buttons: List[str]) -> ReplyKeyboardMarkup:
+        markup = ReplyKeyboardMarkup(row_width=2)
+        markup.add(*[KeyboardButton(i) for i in buttons])
+        return markup
 
-
-@get_keyboard_buttons("settings")
-def settings_keyboard(buttons: List[str] = []) -> ReplyKeyboardMarkup:
-    markup = ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
-    markup.add(*[KeyboardButton(i) for i in buttons])
-    return markup
+    return gen_markup
 
 
-if __name__ == "__main__":
-    basic_start_keyboard()
+# give descriptive names for each keyboard
+basic_start_keyboard = get_keyboard_markup("basic")
+settings_keyboard = get_keyboard_markup("settings")
+cancel_done_keyboard = get_keyboard_markup("cancel_done")
+daily_feed_keyboard = get_keyboard_markup("daily_feed")
+yes_no_keyboard = get_keyboard_markup("yes_no")
